506235d85d4443db80ac9d67f03822f388ac9582
Dan-Burns <dnlburns808@gmail.com>
2025-06-12 18:57:13 -0600

    switch to TrajectoryJob class

--- chacra/trajectories/process_hremd.py
+++ chacra/trajectories/process_hremd.py
@@ -8,8 +8,7 @@ from ..utils import get_resources
 from multiprocessing import Pool
 from pathlib import Path
 import sys
-
-#sys.getsizeof()
+from dataclasses import dataclass
 
 def load_femto_data(hremd_data:str|os.PathLike)-> pd.DataFrame:
     '''
@@ -115,46 +114,77 @@ def get_traj_size(structure:str|os.PathLike,
 def determine_max_jobs(mem_per_job_gb: float,
                        available_mem_gb: float, 
                        reserve_pct: float = 0.2) -> int:
+    
     usable_mem = available_mem_gb * (1 - reserve_pct)
+
     return max(1, int(usable_mem // mem_per_job_gb))
+
+@dataclass
+class TrajectoryJob:
+    '''
+    A dataclass to hold the parameters for a trajectory job.
+    Parameters
+    ----------
+    structure : str
+        Path to simulation topology / pdb.
+    traj_dir : str
+        Path to directory with the femto hremd trajectories.
+    hremd_data : dict
+        A dictionary with state id keys and list of lists containing 
+        each replica's frames that correspond to the state.
+    save_interval : int
+        The number of cycles that elapse between writing coordinates.
+        femto.md.config.HREMD(trajectory_interval=save_interval)
+    output_dir : str
+        Path to the directory where the individual state trajectories will be 
+        written.
+    selection : str, optional
+        An MDAnalysis selection of the atoms that should be used. Default is 
+        'protein'.
+    ref : str | None, optional
+        Path to reference structure. If ref is provided, coordinates are aligned 
+        to C-alphas of selection. Default is None and coordinates will be aligned 
+        to C-alphas of original structure.
+    state_index : int, optional
+        Index of the state to write. Default is None.
+    replica_index : int, optional
+        Index of the replica to write. Default is None.
+    n_states : int | None, optional
+        The number of thermodynamic states in the HREMD simulation. If None, 
+        will be determined from the hremd_data. Default is None.
+    '''
+    structure: str
+    traj_dir: str
+    hremd_data: str | dict
+    save_interval: int = None
+    output_dir: str = None
+    selection: str = 'protein'
+    ref: str | None = None
+    state_index: int = None
+    replica_index: int = None
+    n_states: int | None = None
+
+
 
 ################# Functions called from the ReplicaHandler class ###############
 
-
-def get_state_coordinates_from_replica(structure:str|os.PathLike, 
-                             traj_dir:str|os.PathLike, 
-                             hremd_data:str|os.PathLike|dict, 
-                             save_interval:int,  
-                             selection:str='protein',
-                             replica_index:int=None,
-                             state_index:int=None) -> np.ndarray:
+def get_state_coordinates_from_replica(job: TrajectoryJob) -> np.ndarray:
     '''
     Get the frames from all replicas that correspond to a specific state.
 
     Parameters
     ----------
-    structure : str
-        Path to simulation topology / pdb.
-
-    traj_dir : str
-        Path to directory with the femto hremd trajectories.
-
-    hremd_data : str
-        Path to the femto state data output (i.e. samples.arrow file).
-        Or the state_replica_frames dictionary.
-
-    save_interval : int
-        The number of cycles that elapse between writing coordinates.
-
-    selection : str, optional
-        An MDAnalysis selection of the atoms that should be used. Default is 
-        'protein'.
-
-    replica_index : int
-        Index of the replica to get frames from.
-
-    state_index : int, optional
-        Index of the state to get frames for.
+    job : TrajectoryJob
+        A dataclass containing the parameters for the trajectory job.
+        TrajectoryJob(
+                            structure=structure,
+                            traj_dir=traj_dir,
+                            hremd_data=state_replica_frames,
+                            save_interval=save_interval,
+                            selection=selection,
+                            replica_index=i, 
+                            state_index=state
+                            )
 
     Returns
     -------
@@ -162,236 +192,94 @@ def get_state_coordinates_from_replica(structure:str|os.PathLike,
         Stacked array of frames from all replicas that correspond to the 
         specified state.
     '''
-    structure = str(structure)
-    rep = replica_index
-    u = mda.Universe(structure, str(Path(traj_dir)/f"r{rep}.dcd"))
-    traj_len = len(u.trajectory)
-    sel = u.select_atoms(selection)
-    n_atoms = sel.n_atoms
-    if isinstance(hremd_data, (str, os.PathLike)):
-        df = load_femto_data(hremd_data)
-        state_replica_frames = sort_replica_trajectories(df, 
-                                                        save_interval, traj_len)
-    else:
-        state_replica_frames = hremd_data
 
-    state = state_index 
-    state_traj_len = len(state_replica_frames[rep][state])
+    u = mda.Universe(job.structure, 
+                     str(Path(job.traj_dir)/f"r{job.replica_index}.dcd"))
+    sel = u.select_atoms(job.selection)
+    n_atoms = sel.n_atoms
+
+    state = job.state_index 
+    state_traj_len = len(job.hremd_data[job.replica_index][state])
 
     if state_traj_len == 0:
         return None
     else:
         coordinates = np.zeros((state_traj_len,n_atoms,3))
     
-        for i,frame in enumerate(state_replica_frames[rep][state]):
+        for i,frame in enumerate(job.hremd_data[job.replica_index][state]):
             u.trajectory[frame]
             coordinates[i] = sel.positions 
         
         return coordinates
     
 
-
-    
-    
-    
-    
-def write_state_trajectory(structure:str|os.PathLike, 
-                             traj_dir:str|os.PathLike, 
-                             hremd_data:str|os.PathLike|dict, 
-                             save_interval:int,  
-                             output_dir:str, 
-                             selection:str='protein',
-                             ref=None,
-                             state_index=None):
+def write_state_trajectory(job:TrajectoryJob):
     '''
     Separate the state trajectory for a thermodynamic state from the femto HREMD
     replica trajectories.
     
     Parameters
     ----------
-    structure : str
+    job : TrajectoryJob
+        A dataclass containing the parameters for the trajectory job.
         Path to simulation topology / pdb.
 
-    traj_dir : str
-        Path to directory with the femto hremd trajectories.
-
-    hremd_data : str
-        Path to the femto state data output (i.e. samples.arrow file).
-
-    save_interval : int
-        The number of cycles that elapse between writing coordinates.
-
-    output_dir : str
-        Path to the directory where the individual state trajectories will be 
-        written.
-
-    selection : str, optional
-        An MDAnalysis selection of the atoms that should be used. Default is 
-        'protein'.
-
-    ref : str, optional
-        Path to reference structure. If ref is provided, coordinates are aligned
-        to C-alphas of selection. Default is None.
-
-    state_index : int
-        Index of the state to write. 
+    TrajectoryJob(
+                            structure=structure,
+                            traj_dir=traj_dir,
+                            hremd_data=state_replica_frames,
+                            save_interval=save_interval,
+                            output_dir=output_dir,
+                            selection=selection,
+                            ref=ref,
+                            state_index=i,
+                            n_states=self.n_states
+                            ) 
 
     Returns
     -------
     Writes state trajectory for state_index to output_dir.
     
     '''
-    structure = str(structure)
-    ref = str(ref) if ref is not None else None
-    traj = [file for file in os.listdir(traj_dir) if (file.endswith("dcd"))][0]
-    u = mda.Universe(structure, str(Path(traj_dir)/traj))
-    traj_len = len(u.trajectory)
+    
+    traj = [file for file in os.listdir(job.traj_dir) if (file.endswith("dcd"))][0]
+    u = mda.Universe(job.structure, str(Path(job.traj_dir)/traj))
+    
 
-    if isinstance(hremd_data, (str, os.PathLike)):
-        df = load_femto_data(hremd_data)
-        state_replica_frames = sort_replica_trajectories(df, 
-                                                         save_interval, 
-                                                         traj_len)
+    if job.ref is not None:
+        ref = mda.Universe(job.ref)
     else:
-        state_replica_frames = hremd_data
-    n_states = len(state_replica_frames)
+        ref = mda.Universe(job.structure)
 
-    if ref is not None:
-        ref = mda.Universe(ref)
-    else:
-        ref = mda.Universe(structure)
 
-    state = state_index
+    out_u = mda.Universe(job.structure)
+    sel = out_u.select_atoms(job.selection)
 
-    out_u = mda.Universe(structure)
-    sel = out_u.select_atoms(selection)
-
-    coords = [get_state_coordinates_from_replica(structure, 
-                             traj_dir, 
-                             state_replica_frames,
-                             save_interval,  
-                             selection='protein',
-                             replica_index=i,
-                             state_index=state)
-
-                             for i in range(n_states)
+    coords = [get_state_coordinates_from_replica(
+                TrajectoryJob(
+                            structure=job.structure, 
+                            traj_dir=job.traj_dir, 
+                            hremd_data=job.hremd_data,
+                            save_interval=job.save_interval,  
+                            selection=job.selection,
+                            replica_index=i,
+                            state_index=job.state_index
+                            )
+                            )
+                             for i in range(job.n_states)
                              ]
     coords = [c for c in coords if c is not None]
     coordinates = np.concatenate(coords, axis=0)
     # These coordinates will not match write_state_trajectories because they are 
     # added sequentially per replica and not into the array index corresponding 
     # to the frame they were taken from. 
-    
 
-    with mda.Writer(str(Path(output_dir)/f"state_{state}.xtc"), 
+    with mda.Writer(str(Path(job.output_dir)/f"state_{job.state_index}.xtc"), 
                     n_atoms=sel.n_atoms) as writer:
         for i in range(coordinates.shape[0]):
             sel.positions = coordinates[i] 
-            align.alignto(sel, ref, select=f"({selection}) and name CA")
+            align.alignto(sel, ref, select=f"({job.selection}) and name CA")
             writer.write(sel.atoms) 
-
-def write_state_trajectory_parallel(structure:str|os.PathLike, 
-                             traj_dir:str|os.PathLike, 
-                             hremd_data:str|os.PathLike|dict, 
-                             save_interval:int,  
-                             output_dir:str|os.PathLike, 
-                             selection:str='protein',
-                             ref=None,
-                             state_index=None,
-                             n_jobs=None):
-    
-    '''
-    Separate the state trajectory for a thermodynamic state from the femto HREMD
-    replica trajectories. Each replica is assigned to a single separate process.
-    
-    Parameters
-    ----------
-    structure : str
-        Path to simulation topology / pdb.
-
-    traj_dir : str
-        Path to directory with the femto hremd trajectories.
-
-    hremd_data : str
-        Path to the femto state data output (i.e. samples.arrow file).
-
-    save_interval : int
-        The number of cycles that elapse between writing coordinates.
-
-    output_dir : str
-        Path to the directory where the individual state trajectories will be 
-        written.
-
-    selection : str, optional
-        An MDAnalysis selection of the atoms that should be used. Default is 
-        'protein'.
-
-    ref : str, optional
-        Path to reference structure. If ref is provided, coordinates are aligned 
-        to C-alphas of selection. Default is None.
-
-    state_index : int
-        Index of the state to write. 
-
-    Returns
-    -------
-    Writes state trajectory for state_index to output_dir.
-    
-    '''
-    structure = str(structure)
-    ref = str(ref) if ref is not None else None
-    traj = [file for file in os.listdir(traj_dir) if (file.endswith("dcd"))][0]
-    u = mda.Universe(structure, Path(traj_dir)/traj)
-    traj_len = len(u.trajectory)
-
-    
-    if isinstance(hremd_data, (str, os.PathLike)):
-        df = load_femto_data(hremd_data)
-        state_replica_frames = sort_replica_trajectories(df, save_interval, 
-                                                         traj_len)
-    else:
-        state_replica_frames = hremd_data
-    n_replicas = len(state_replica_frames)
-
-    if ref is not None:
-        ref = mda.Universe(ref)
-    else:
-        ref = mda.Universe(structure)
-
-    state = state_index
-
-    out_u = mda.Universe(structure)
-    sel = out_u.select_atoms(selection)
-
-    coords_args_list = [(structure, 
-                        traj_dir,
-                        hremd_data,
-                        save_interval, 
-                        selection,
-                        i, 
-                        state)
-                        for i in range(n_replicas)]
-    if n_jobs is None:
-        resources = get_resources()
-        n_jobs = resources['num_cores']
-    with Pool(n_jobs) as worker_pool:
-        coords = worker_pool.starmap(get_state_coordinates_from_replica,
-                                     coords_args_list)
-
-    coords = [c for c in coords if c is not None]
-    coordinates = np.concatenate(coords, axis=0)
-    # These coordinates will not match write_state_trajectories because they are 
-    # added sequentially per replica and not into the array index corresponding to 
-    # the frame they were taken from. If contact frequencies match - it's correct.
-
-    with mda.Writer(str(Path(output_dir)/f"state_{state}.xtc"), 
-                    n_atoms=sel.n_atoms) as writer:
-        for i in range(coordinates.shape[0]):
-            sel.positions = coordinates[i]  
-            align.alignto(sel, ref, select=f"({selection}) and name CA")
-            writer.write(sel.atoms) 
-
-
 
 
 class ReplicaHandler:
@@ -437,11 +325,11 @@ class ReplicaHandler:
     
 
     def write_state_trajectory(self, 
-                                        output_dir:str|os.PathLike, 
-                                        selection:str='protein',
-                                        ref=None,
-                                        state_index=None,
-                                        n_jobs=None):
+                                output_dir:str|os.PathLike, 
+                                selection:str='protein',
+                                ref=None,
+                                state_index=None,
+                                n_jobs=None):
     
         '''
         Separate the state trajectory for a thermodynamic state from the femto 
@@ -483,19 +371,23 @@ class ReplicaHandler:
         out_u = mda.Universe(self.structure)
         sel = out_u.select_atoms(selection)
 
-        coords_args_list = [(self.structure,
-                            self.traj_dir,
-                            self.state_replica_frames,
-                            self.save_interval,
-                            selection,
-                            i, 
-                            state)
+        coords_args_list = [
+                            TrajectoryJob(
+                            structure=self.structure,
+                            traj_dir=self.traj_dir,
+                            hremd_data=self.state_replica_frames,
+                            save_interval=self.save_interval,
+                            selection=selection,
+                            replica_index=i, 
+                            state_index=state
+                            )
+
                             for i in range(self.n_states)]
         
         if n_jobs is None:
             n_jobs = self.resources['num_cores']
         with Pool(n_jobs) as worker_pool:
-            coords = worker_pool.starmap(get_state_coordinates_from_replica,
+            coords = worker_pool.map(get_state_coordinates_from_replica,
                                         coords_args_list)
 
         coords = [c for c in coords if c is not None]
@@ -540,30 +432,34 @@ class ReplicaHandler:
             aligned to C-alphas of selection.
         '''
 
-        state_args_list = [(self.structure,
-                            self.traj_dir,
-                            self.state_replica_frames,
-                            self.save_interval,
-                            output_dir,
-                            selection,
-                            ref,
-                            i) 
-                            for i in range(self.n_states)] 
-
+        state_args_list = [
+                            TrajectoryJob(
+                            structure=self.structure,
+                            traj_dir=self.traj_dir,
+                            hremd_data=self.state_replica_frames,
+                            save_interval=self.save_interval,
+                            output_dir=output_dir,
+                            selection=selection,
+                            ref=ref,
+                            state_index=i,
+                            n_states=self.n_states
+                            ) 
+                            for i in range(self.n_states)
+                            ] 
 
         if n_jobs is None:
             n_jobs = self.resources['num_cores']  
-        # print(f"Attempting {n_jobs} jobs for writing state trajectories.")
-
-        # because each job needs enough memory to hold the entire trajectory,
-        # need to check available resources and set n_jobs accordingly.
+        
         required_memory_gb = get_traj_size(self.structure,
                                        str(Path(self.traj_dir)/"r0.dcd"),
                                        selection=selection) /1e9
-        # print(required_memory_gb)
+        
         max_jobs = determine_max_jobs(required_memory_gb,
                                         self.resources['available_ram_gb'],
                                         reserve_pct=0.1)
+        # TODO: If you're memory limited, is it faster to run one state at
+        # with all cores processing that one state, or to run as many cores
+        # as max jobs permits with one core per state?
         # if n_jobs > max_jobs:
             # # Then just use all cores for one state at a time.
             # [self.write_state_trajectory_parallel(output_dir, 
@@ -572,12 +468,10 @@ class ReplicaHandler:
             #                                       i) 
             #                          for i in range(self.n_states)]
         n_jobs = min(n_jobs, max_jobs)
-        # print(f"Using {n_jobs} jobs for writing state trajectories.")
         # run as many states as cores simultaneously
 
-        # Apparently for Pool, the function cannot be a method of a class
         with Pool(n_jobs) as worker_pool:
-            worker_pool.starmap(write_state_trajectory,
+            worker_pool.map(write_state_trajectory,
                                         state_args_list)
             
 
