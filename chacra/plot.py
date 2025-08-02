b91e6c8e32eccd3863d1f9efb68f43bd0f3c2550
Dan-Burns <dnlburns808@gmail.com>
2025-08-01 19:27:51 -0600

    clean up

--- chacra/plot.py
+++ chacra/plot.py
@@ -6,7 +6,11 @@ from scipy.interpolate import make_interp_spline
 from .visualize.colors import *
 from itertools import combinations
 
-def plot_difference_of_roots(cpca,n_pcs=None,filename=None, dot_color=cherenkov_blue, cutoff_color='r'):
+def plot_difference_of_roots(cpca,
+                             n_pcs=None,
+                             filename=None, 
+                             dot_color=cherenkov_blue, 
+                             cutoff_color='r'):
     '''
     Plot the difference of roots test results
     Parameters
@@ -22,7 +26,7 @@ def plot_difference_of_roots(cpca,n_pcs=None,filename=None, dot_color=cherenkov_
     filename : str
         If saving the file, provide the filename.
     '''
-    #TODO automatically run _permutated_ since it's way faster now.
+   
     if cpca._permutated_explained_variance is None:
         print("You have to run the ContactPCA.permutated_pca \n method before "
               "you can plot the difference of roots results. ")
@@ -35,15 +39,22 @@ def plot_difference_of_roots(cpca,n_pcs=None,filename=None, dot_color=cherenkov_
         # difference of roots
         # can just do cpca.chacra_pvals 
         p_val = np.sum(np.abs(np.diff(variance, axis=1, prepend=0)) > \
-                    np.abs(np.diff(original_variance, prepend=0)), axis=0) / cpca._N_permutations
+                    np.abs(np.diff(original_variance, prepend=0)), axis=0) / \
+                        cpca._N_permutations
         ax.hlines(.05,xmin=0,xmax=n_pcs,color=cutoff_color,zorder=1)
-        ax.scatter([f'{i+1}' for i in range(n_pcs)], p_val[:n_pcs], color=dot_color, label='p-value on significance')
+        ax.scatter([f'{i+1}' for i in range(n_pcs)], p_val[:n_pcs], 
+                   color=dot_color, label='p-value on significance')
         if filename is not None:
             fig.savefig(filename)
     
-def plot_chacras(cpca, n_pcs=4, contacts=None, temps=None, colors=chacra_colors,
-                 spacing='geometric', temp_scale=None,
-                  filename=None):
+def plot_chacras(cpca, 
+                 n_pcs=4, 
+                 contacts=None, 
+                 temps=None, 
+                 colors=chacra_colors,
+                 spacing='geometric', 
+                 temp_scale=None,
+                 filename=None):
     '''
     cpca : ContactPCA
         The ContactPCA object.
@@ -106,46 +117,24 @@ def plot_chacras(cpca, n_pcs=4, contacts=None, temps=None, colors=chacra_colors,
         ax.set_xlabel(f"Temperature ({temp_scale})", fontsize=12)
     ax.set_ylabel("Projection", fontsize=12)
     #ax.vlines(x=373,ymin=0,ymax=.84,linestyles='dotted')
-    ax.legend([f'PC{i}' for i in range(1,n_pcs+1)], fontsize=12, loc='lower center', ncol=2)
+    ax.legend([f'PC{i}' for i in range(1,n_pcs+1)], fontsize=12, 
+              loc='lower center', ncol=2)
     if filename:
         fig.savefig(filename)
 
-def plot_split_sums(cpca, pc, label_top=10, filename=None):
-    '''
-    Scatter plot of the sums of the loading score splits (see ContactFrequencies.get_split_sums)
-
-    Parameters
-    ----------
-
-    cpca : ChACRA.ContactAnalysis.ContactPCA
-
-    pc : The pc for the data you want to plot
-
-    label_top : int
-
-    filename : path to output file
-    '''
-    vals = cpca.score_sums.loc[pc].values
-    top_labels = cpca.score_sums.loc[pc][cpca.score_sums.loc[pc] > cpca.score_sums.loc[pc].sort_values()[-label_top]].index
-    label_indices = np.argwhere(np.isin(cpca.score_sums.loc[pc].index, top_labels)==True).flatten()
-
-    fig, ax = plt.subplots()
-    ax.scatter(range(vals.shape[0]),vals)
-    [ax.text(h, j, top_labels[-label_top:][k], **{'fontsize':'x-small'}) 
-                        for k,(h,j) in enumerate(zip(label_indices,vals[label_indices]))]
-    if filename is not None:
-        fig.savefig(filename)
-
-
-def biplots(cpca, pcs=list(range(1,5)), label_top=None, colors=chacra_colors, filename=None):
+def biplots(cpca, 
+            pcs=list(range(1,5)), 
+            label_top=None, 
+            colors=chacra_colors, 
+            filename=None):
     '''
     NOT IMPLEMENTED 
     
-    option to label outliers/ corners/ top loading scores and color on a mixing gradient
-    so something that's in a corner of a pc1-pc2 biplot appears purple
+    option to label outliers/ corners/ top loading scores and color on a mixing 
+    gradient so something that's in a corner of a pc1-pc2 biplot appears purple
 
-    quick way to identify or look for correlation with known functionally important residues and
-    suggest where connections between modes occur.
+    quick way to identify or look for correlation with known functionally 
+    important residues and suggest where connections between modes occur.
     
     Parameters
     ----------
@@ -155,7 +144,8 @@ def biplots(cpca, pcs=list(range(1,5)), label_top=None, colors=chacra_colors, fi
     label_top : int
         number of top scoring contacts to display labels for on the plot
     '''
-    # TODO when you add labels, the figure size changes (probably because of "constrained_layout")
+    # TODO when you add labels, the figure size changes 
+    # (probably because of "constrained_layout")
     # if pcs is list of int, make all the combos
     if type(pcs[0]) == int:
         combos = list(combinations(pcs,2))
@@ -169,16 +159,19 @@ def biplots(cpca, pcs=list(range(1,5)), label_top=None, colors=chacra_colors, fi
     n_rows = int(np.ceil(n_plots/4))
     # remove the last n_plot ax
     n_axes_to_remove = 4 - int(n_plots % 4)
-    fig, axs = plt.subplots(n_rows, 4, figsize=(15,3*n_rows), constrained_layout=True)
+    fig, axs = plt.subplots(n_rows, 4, figsize=(15,3*n_rows), 
+                            constrained_layout=True)
     for i, combo in enumerate(combos):
         pc_a = combo[0]
         pc_b = combo[1]
         # get the color gradient from transparent white to the pc's hex code
-        # TODO Everything goes totally transparent and colors are all red now.....
-        a_grad = get_color_gradient('#ffffff00',colors[pc_a-1],100, return_rgb=True,alpha=True)
-        b_grad = get_color_gradient('#ffffff00',colors[pc_b-1],100, return_rgb=True, alpha=True)
+        # TODO Everything goes totally transparent and colors are all red now...
+        a_grad = get_color_gradient('#ffffff00',colors[pc_a-1],100, 
+                                    return_rgb=True,alpha=True)
+        b_grad = get_color_gradient('#ffffff00',colors[pc_b-1],100, 
+                                    return_rgb=True, alpha=True)
 
-        # use for indexing the list of colors by multiplying by the normalized loading score 
+        # indexing the list of colors by the normalized loading score 
         a_grad_len = len(a_grad)-1
         b_grad_len = len(b_grad)-1
 
@@ -189,7 +182,8 @@ def biplots(cpca, pcs=list(range(1,5)), label_top=None, colors=chacra_colors, fi
         val_2 = cpca.sorted_norm_loadings(pc_a)[f'PC{pc_b}'].values
         # mix the two colors for all of the points
         colors = [rgb_to_hex(mix_color(
-                                    a_grad[round(val[0]*a_grad_len)], b_grad[round(val[1]*b_grad_len)]
+                                    a_grad[round(val[0]*a_grad_len)], 
+                                    b_grad[round(val[1]*b_grad_len)]
                                         )
                             ) 
                 for val in zip(val_1,val_2)
@@ -245,7 +239,8 @@ fig.tight_layout()
 def plot_energies(energies, filename=None, n_bins=20):
     ''''
     energies : np.array
-        The n_cycles x n_states array of energies from the replica exchange simulations.
+        The n_cycles x n_states array of energies from the replica exchange 
+        simulations.
     
     Returns
     -------
