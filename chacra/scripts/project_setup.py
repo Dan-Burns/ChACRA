801593a979462cd98841a4668cf90d573786703b
Dan-Burns <dnlburns808@gmail.com>
2025-08-09 14:17:17 -0600

    option to copy example file to working directory.

--- chacra/scripts/project_setup.py
+++ chacra/scripts/project_setup.py
@@ -1,7 +1,20 @@
 import os
-
+import argparse
+from pathlib import Path
+import shutil
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Set up ChACRA project directories.")
+    parser.add_argument(
+        "--example",
+        action="store_true",
+        help="Include the example file.",
+        default=False
+        )
+    
+    args = parser.parse_args()
+
     dirs = [
         "analysis_output",
         "contact_output",
@@ -13,7 +26,9 @@ def main():
     ]
     for directory in dirs:
         os.makedirs(directory, exist_ok=True)
-
+    if args.example:
+        EXAMPLE_PDB = Path(__file__).parent.parent / "examples" / "1tnf.pdb"
+        shutil.copy(EXAMPLE_PDB, "structures/1tnf.pdb")
 
 if __name__ == "__main__":
     main()
